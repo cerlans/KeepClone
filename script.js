d6d70373311e1f9a61e172719a70e8df8cd5609d
cerlans <kgomez222@hotmail.com>
2021-09-27 14:42:38 -0400

    2 files modified

--- script.js
+++ script.js
@@ -2,8 +2,9 @@ noteField = document.getElementById('noteField');
 noteTitle = document.getElementById('noteTitle');
 trueForm = document.getElementById('trueForm');
 submitButton = document.getElementById('submitButton');
-noteArea = document.getElementById('noteArea')
-console.log(noteTitle)
+noteArea = document.getElementById('noteArea');
+console.log(noteTitle);
+
 
 noteField.addEventListener('focus',()=>{
   noteTitle.style.display='block';
@@ -23,9 +24,10 @@ submitButton.addEventListener('click',()=>{
 //Parent of all the notes that will be added
 noteArea.addEventListener('click',(event)=>{
 
-  if(event.target.className === 'far fa-edit'){
-    console.log(event.target)
-    //createModal('div','modal')
+  if(event.target.className === 'parentNote'){
+    let dynamicTitle = event.target.children[0].innerText;
+    let dynamicNoteDescription = event.target.children[1].innerText;
+    createModal('div','modal',dynamicTitle,dynamicNoteDescription)
   }
 })
 //bchiang creates a 'backdrop' sort off like the modal from w3 schools that acts as the anchor to close the text field when its clicked out 
@@ -38,6 +40,7 @@ function createNote(noteText,noteTitle){
   const innerDiv = document.createElement('div')
   const secondDiv = document.createElement('div')
   const littleIcon = document.createElement('i')
+  secondDiv.classList.add('iconParent')
   littleIcon.classList.add('far','fa-edit')
   secondDiv.append(littleIcon)
   innerDiv.innerText = noteTitle
@@ -57,17 +60,41 @@ function reset (){
   noteTitle.value=''
 }
 
-function createModal(type,firstClass,){
+function createModal(type,firstClass,title,noteDescription){
   //div in this case
   //is there a faster way to specify i want 3 divs created?
   let hiddenModal= document.createElement(type);
+
   let childModal = document.createElement(type);
-  let firstChild = document.createElement(type);
-  let secondChild = document.createElement(type);
-  childModal.classList.add('childModal')
+  childModal.classList.add('childModal');
+
+  let firstChild = document.createElement('input');
+  firstChild.classList.add('modalNoteTitle');
+  firstChild.value = title
+
+  let secondChild = document.createElement('textarea');
+  secondChild.classList.add('modalNoteDescription')
+  secondChild.innerText = noteDescription
+
+  let footerSection = document.createElement(type);
+  footerSection.classList.add('modalFooter');
+
+  let footerChildOne = document.createElement(type);
+  footerChildOne.classList.add('fas','fa-trash');
+  let footerChildtwo = document.createElement(type);
+  let footerButton = document.createElement('button')
+  footerButton.classList.add('footerButton')
+  footerButton.innerHTML = 'Done'
+  footerChildtwo.append(footerButton);
+  
   childModal.append(firstChild);
   childModal.append(secondChild);
   hiddenModal.append(childModal)
+
+  footerSection.append(footerChildOne);
+  footerSection.append(footerChildtwo);
+  childModal.append(footerSection);
+
   hiddenModal.classList.add(firstClass)
   document.body.append(hiddenModal)
   }
